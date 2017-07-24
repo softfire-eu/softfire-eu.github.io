357003d1f925de6d78720d49ba1405bdcde2b62c
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-07-24 18:15:58 +0200

    add extra js file

---
+++ docs/javascripts/extra.js
@@ -0,0 +1,15 @@
+<!---
+Script for open external links in a new tab
+-->
+<script type="text/javascript" charset="utf-8">
+      // Creating custom :external selector
+      $.expr[':'].external = function(obj){
+          return !obj.href.match(/^mailto\:/)
+                  && (obj.hostname != location.hostname);
+      };
+      $(function(){
+        $('a:external').addClass('external');
+        $(".external").attr('target','_blank');
+      })
+</script>
+
