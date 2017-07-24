42242515b61fe865b578a5995570aa80a461cba1
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-07-24 18:50:05 +0200

    add import

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,6 +1,7 @@
 <!---
 Script for open external links in a new tab
 -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script type="text/javascript" charset="utf-8">
       // Creating custom :external selector
       $.expr[':'].external = function(obj){
